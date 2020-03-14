4ad312827ea5352cd5c6839f61628aa0f94b8bd8
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2020-03-14 15:14:43 -0500

    findAllOfficers test added for JPA

--- src/test/java/com/galvanize/repositories/JpaOfficerDaoTest.java
+++ src/test/java/com/galvanize/repositories/JpaOfficerDaoTest.java
@@ -1,8 +1,11 @@
 package com.galvanize.repositories;
 
+import com.galvanize.entities.Officer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +19,12 @@ class JpaOfficerDaoTest {
         Long count = jpaOfficerDao.count();
         assertEquals(5L, count);
     }
+    @Test
+    void findAllOfficers() {
+        List<Officer> officers = jpaOfficerDao.findAll();
+        assertFalse(officers.isEmpty());
+    }
+
+    
 
 }
